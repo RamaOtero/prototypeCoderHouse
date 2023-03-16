3012ea2281bcea5a2ddacd3deab9848c797e2eee
Ramiro Otero <ramirootero98@gmail.com>
2023-03-15 22:53:46 -0300

    primer pre entrega

---
+++ src/components/NavBar/navBar.js
@@ -0,0 +1,15 @@
+import CartWidget from "../cartWidget/cartWidget"
+
+const navBar = () => {
+    return (
+    <nav>
+      <div>
+         <h1>Prototype</h1>
+      </div>
+        <CartWidget />
+    </nav>
+
+    )
+}
+
+    export default navBar
---
+++ src/components/cartWidget/cartWidget.js
@@ -0,0 +1,12 @@
+import Cart from './assets/cart.png'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img  src={Cart} alt='cart-widget' />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
